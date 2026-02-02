68dbc4d5ee05141d470c53a15b9f6d2c2a8b6cf4
belemirdemirkol <belemirdemirkol@gmail.com>
2026-02-02 19:17:38 +0000

    Update BMI CALCULATOR.py

--- BMI CALCULATOR.py
+++ BMI CALCULATOR.py
@@ -1,44 +1,46 @@
 import tkinter
 window=tkinter.Tk()
-window.title("MY BMI CALCULATOR")
-window.minsize(width=500, height=500)
+window.title("BMI CALCULATOR")
+window.geometry("500x500")
 window.config(bg="pink")
 
-def clickt_button():
-    try:
-        user_input=my_entry.get()
-        user_input2=my_entry2.get()
-        weight=float(user_input)
-        height=float(user_input2)/100
-        bmi=weight/(height*height)
-        if bmi<18:
-            my_label3.config(text="You are underweight")
-        elif bmi<25:
-            my_label3.config(text="You are normal")
-        elif bmi<30:
-            my_label3.config(text="You are overweight")
-        elif bmi<35:
-            my_label3.config(text="You are obese")
-        else:
-            my_label3.config(text="NO COMMENT!!!")
-    except ValueError:
-        my_label3.config(text="Please enter a number")
+def click_button():
+         weight_input=weight_entry.get()
+         height_input=height_entry.get()
+         result_entry.delete(0, tkinter.END)
+         try:
+             w=float(weight_input)
+             h=float(height_input)/100
+             BMI=w/(h*h)
 
-my_label=tkinter.Label(text="ENTER YOUR WEIGHT")
-my_label.config(bg="pink",fg="white",font=("Times New Roman",18,"bold"))
-my_label.pack()
-my_entry=tkinter.Entry(width=30)
-my_entry.pack()
-my_label2=tkinter.Label(text="ENTER YOUR HEIGHT")
-my_label2.config(bg="pink",fg="white",font=("Times New Roman",18,"bold"))
-my_label2.pack()
-my_entry2=tkinter.Entry(width=30)
-my_entry2.pack()
-my_label3=tkinter.Label(text="YOUR RESULT")
-my_label3.config(bg="pink",fg="white",font=("Times New Roman",18,"bold"))
-my_label3.pack()
-my_button=tkinter.Button(text="Click Me",command=clickt_button)
-my_button.pack()
+             if BMI<18.5:
+                 result_entry.insert(0,f"BMI: {BMI:.2f}-You are underweight")
+             elif 18.5<=BMI<25:
+                 result_entry.insert(0,f"BMI: {BMI:.2f}-You are normal")
+             elif 25<=BMI<30:
+                 result_entry.insert(0,f"BMI: {BMI:.2f}-You are overweight")
+             elif 30<=BMI<40:
+                 result_entry.insert(0,f"BMI: {BMI:.2f}-You are obese")
+             else:
+                 result_entry.insert(0,"NO COMMENT!!!")
+
+         except ValueError:
+             result_entry.insert(0,"Please enter both weight and height correctly")
+
+
+
+weight_label=tkinter.Label(text="Please Enter Your Weight",bg="pink",fg="black",font=("Times New Roman",25))
+weight_entry=tkinter.Entry(width=15,font=("Times New Roman",15,))
+weight_label.pack()
+weight_entry.pack()
+height_label=tkinter.Label(text="Please Enter Your Height",bg="pink",fg="black",font=("Times New Roman",25))
+height_entry=tkinter.Entry(width=15,font=("Times New Roman",15))
+height_label.pack()
+height_entry.pack()
+button=tkinter.Button(text="Click for Result",command=click_button)
+result_entry=tkinter.Entry(width=34,font=("Times New Roman",15))
+button.pack(pady=20)
+result_entry.pack()
 
 window.mainloop()
 
